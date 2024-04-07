a1b21db76e76cbc6b47c9267ce4fb2b864e0f2db
Pingquan2024 <esrgfu@gmail.com>
2024-04-07 23:10:07 +0800

    2024年4月7日链表部分代码

---
+++ Code_2024/SList/SList.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdio.h>
+
+//Á´±í
+typedef int SLTDataType;
+
+typedef struct SListNode
+{
+	struct SListNode* next;
+	SLTDataType data;
+}SLTNode;
+
+void SLTPrint(SLTNode* phead);
+void SLPushFront(SLTNode** pphead, SLTDataType x);
+void SLPushBack(SLTNode** pphead, SLTDataType x);
+
+void SLPopFront(SLTNode** pphead);
+void SLPopBack(SLTNode** pphead);
--- Code_2024/test_3_14/Text-adventure.h
+++ Code_2024/test_3_14/Text-adventure.h
@@ -1,26 +1,43 @@
 #pragma once
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+//
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//#ifndef TEXT_ADVENTURE_H
+//#define TEXT_ADVENTURE_H
+//
+//#define ROWS 5
+//#define COLS 5
+//
+//enum GraDifficulty
+//{
+//	JuniorPlayer =1,
+//	IntermediatePlayer,
+//	AdvancedPlayer
+//
+//};
+//
+////函数声明Function declarations
+//
+////初始化函数
+//void InitializeGame(char board[ROWS][COLS], int numTreasures, int numTraps);
+//
+////打印棋盘
+//void DisplayMap(char board[ROWS][COLS]);
+//
+////玩家移动
+//int MovePlayer(char board[ROWS][COLS], char move);
+//
+////检查移动
+//int CheckMove(char board[ROWS][COLS], int newRow, int newCol);
+//
+////检查是否胜利
+//int CheckWin(char board[ROWS][COLS], int numTreasures);
+//
+//#endif
 
-#ifndef GAME_H
-#define GAME_H
 
-// 游戏地图的大小
-#define MAP_SIZE 10
 
-// 地图上的不同元素
-#define EMPTY '0'
-#define PLAYER 'P'   //玩家位置
-#define TREASURE 'T' //宝藏
-#define TRAP 'X'     //陷进
 
-// 函数声明
-void initializeGame(char map[MAP_SIZE][MAP_SIZE]);
-void displayMap(char map[MAP_SIZE][MAP_SIZE]);
-void movePlayer(char map[MAP_SIZE][MAP_SIZE], int* playerX, int* playerY, char move);
-int checkMove(char map[MAP_SIZE][MAP_SIZE], int playerX, int playerY, char move);
-void playGame();
-
-#endif
